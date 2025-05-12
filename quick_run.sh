bef20e4c70786814b2174a77eb5bb9abcb9f4135
jlingford <james.lingford@gmail.com>
2025-05-12 15:09:22 +1000

    docs: update quick_run docs

--- quick_run.sh
+++ quick_run.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/bash
 
+# for quick iterative testing in local environment without Docker.
+
+# create conda env (do once):
+#
+# conda env create -f environment.yml
+# conda activate hyddb_env1
+# pip install parso==0.7.0
+# conda deactivate
+
+# make sure conda env is activated:
+#
+# conda activate hyddb_env1
+
 python manage.py migrate
 python manage.py loaddata hydrogenaseclass hydrogenasesequence
 python manage.py train
