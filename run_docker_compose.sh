1fee5ebd314a639c6e8ceb3b545f49996371b52a
jlingford <james.lingford@gmail.com>
2025-05-14 15:37:37 +1000

    remove unnessary command in run_docker_compose.sh

--- run_docker_compose.sh
+++ run_docker_compose.sh
@@ -3,7 +3,6 @@
 docker compose up -d
 docker compose run web python manage.py makemigrations
 docker compose run web python manage.py migrate
-docker compose run web python manage.py migrate browser
 docker compose run web python manage.py loaddata hydrogenaseclass hydrogenasesequence geneticorganisation
 docker compose run web python manage.py train
 docker compose run web python manage.py trainupstream data
