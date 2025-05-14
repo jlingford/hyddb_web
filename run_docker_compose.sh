c7d4f7392dda81380a2f049d7d6222e249c97497
jlingford <james.lingford@gmail.com>
2025-05-14 13:58:20 +1000

    update run_docker_compose.sh to run with new geneticorganisation fixture

--- run_docker_compose.sh
+++ run_docker_compose.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
 docker compose up -d
+docker compose run web python manage.py makemigrations
 docker compose run web python manage.py migrate
-docker compose run web python manage.py loaddata hydrogenaseclass hydrogenasesequence
+docker compose run web python manage.py migrate browser
+docker compose run web python manage.py loaddata hydrogenaseclass hydrogenasesequence geneticorganisation
 docker compose run web python manage.py train
 docker compose run web python manage.py trainupstream data
